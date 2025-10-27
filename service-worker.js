0101138c4357eaa07882b4ef5222421d39e586eb
koki-mino <fsh.koki.66@gmail.com>
2025-10-28 01:03:08 +0900

    Create  service-worker.js
    サービスワーカーを追加

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+const CACHE = "manabi-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./main.js",
+  "./data/places.geojson",
+  "https://unpkg.com/leaflet/dist/leaflet.css",
+  "https://unpkg.com/leaflet/dist/leaflet.js"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+    ))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
